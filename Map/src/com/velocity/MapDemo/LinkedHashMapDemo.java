02338a46f1f841bd29ac3fbc18379d2a9c3d3b55
nikita <nikitajbhavsar@gmail.com>
2023-07-22 18:22:41 +0530

    LinkedHashMapDemo example

---
+++ Map/src/com/velocity/MapDemo/LinkedHashMapDemo.java
@@ -0,0 +1,35 @@
+package com.velocity.MapDemo;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+public class LinkedHashMapDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedHashMap<Integer,String> maplist=new LinkedHashMap<Integer,String>();
+		
+		maplist.put(11, "dev");
+		maplist.put(16, "dev");
+		maplist.put(12, "Ajay");
+		maplist.put(13, "kajol");
+		maplist.put(15, null);
+		maplist.put(14, "Rani");
+		
+		//System.out.println(maplist);
+		
+		Set<Integer> mp=maplist.keySet();
+		Iterator<Integer> itr =mp.iterator();
+		while(itr.hasNext())
+		{
+			int i=itr.next();
+			System.out.println("Key is:" +i);
+			System.out.println("Value is:" +maplist.get(i));
+			
+		}
+		
+
+	}
+
+}
